ca41bdddf6607cbb470170829d1ffa39b20391a6
Johan Wikman <johan.wikman@mariadb.com>
2015-09-23 15:41:09 +0300

    Made some whitespace fixes.

--- server/core/gateway.c
+++ server/core/gateway.c
@@ -3,18 +3,18 @@
  * software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
  * version 2.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * 
+ *
  * Copyright MariaDB Corporation Ab 2013-2014
- * 
+ *
  */
 
 /**
@@ -28,7 +28,7 @@
  * 12-06-2013	Mark Riddoch		Add the -p option to set the
  * 					listening port
  *					and bind addr is 0.0.0.0
- * 19/06/13	Mark Riddoch		Extract the epoll functionality 
+ * 19/06/13	Mark Riddoch		Extract the epoll functionality
  * 21/06/13	Mark Riddoch		Added initial config support
  * 27/06/13
  * 28/06/13 Vilho Raatikka		Added necessary headers, example functions and
